dc2b5c51995001fc3e34f5799f8598223635c10e
Fraser McCrossan <fraser@mccrossan.ca>
2016-06-21 21:42:38 -0400

    add service list and create Anko UI

--- app/src/main/java/com/coffree/webshare/MainActivity.kt
+++ app/src/main/java/com/coffree/webshare/MainActivity.kt
@@ -8,10 +8,19 @@ import android.util.Log
 import android.widget.Toast
 import java.net.MalformedURLException
 import java.net.URL
+import java.util.*
+import org.jetbrains.anko.*
 
 class MainActivity : AppCompatActivity() {
 
     val TAG = "MainActivity"
+
+    val services: Map<String, (String) -> String> = mapOf(
+            "Facebook" to { u -> "https://www.facebook.com/sharer.php?u=${u}" },
+            "Twitter" to { u -> "https://twitter.com/home?status=${u}" },
+            "Google+" to { u -> "https://plus.google.com/share?url=${u}" },
+            "LinkedIn" to { u -> "https://www.linkedin.com/shareArticle?mini=true&url=${u}&title=&summary=&source=" }
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,13 +34,50 @@ class MainActivity : AppCompatActivity() {
                     i.data = uri
                     startActivity(i)
                     finish()
-                    return // we are done, don't fall through
                 }
                 catch (e : MalformedURLException) {
                     // not a URL, oops
                 }
             }
+        } else {
+            //setContentView(R.layout.activity_main)
+            val vmargin = dip(5)
+            val hmargin = dip(8)
+            val checkHmartin = dip(10)
+            verticalLayout {
+                textView {
+                    textResource = R.string.service_select
+                    textSize = 18f
+                }.lparams(width = matchParent) {
+                    verticalMargin = vmargin
+                    horizontalMargin = hmargin
+                }
+                textView {
+                    textResource = R.string.service_explain
+                    textSize = 14f
+                }.lparams(width = matchParent) {
+                    bottomMargin = vmargin
+                    horizontalMargin = hmargin
+                }
+                verticalLayout {
+                    services.forEach {
+                        val (name, lamb) = it
+                        checkBox(name) {
+                            textSize = 18f
+                            setChecked(getPreferences(MODE_PRIVATE).getBoolean(name, false))
+                            onCheckedChange { button, b ->
+                                getPreferences(MODE_PRIVATE).edit().let { prefs ->
+                                    prefs.putBoolean(name, isChecked)
+                                    prefs.commit()
+                                }
+                            }
+                        }.lparams(width = matchParent) {
+                            topMargin = vmargin
+                            horizontalMargin = checkHmartin
+                        }
+                    }
+                }
+            }
         }
-        setContentView(R.layout.activity_main)
     }
 }
